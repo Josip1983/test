11b0930304a29c15b68c53cf8212cde6820557aa
Josip1983 <cencic.josip@gmail.com>
2024-12-19 18:30:06 +0100

    dodajemo prvi file na github

---
+++ github.py
@@ -0,0 +1,8 @@
+class Ucenik() :
+    def __init__(self, ime, godine, uspjeh) :
+        self.ime=ime
+        self.godine=godine
+        self.uspjeh=uspjeh
+
+ucenik1=Ucenik("Josip", 15, 10)
+print(ucenik1.ime)
