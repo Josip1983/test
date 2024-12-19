7e2a7468fb664bb5cc46d42c761ccf5a089a6127
Josip1983 <cencic.josip@gmail.com>
2024-12-19 19:37:11 +0100

    novi atribut

--- github.py
+++ github.py
@@ -1,8 +1,9 @@
 class Ucenik() :
-    def __init__(self, ime, godine, uspjeh) :
+    def __init__(self, ime, godine, uspjeh, karakter) :
         self.ime=ime
         self.godine=godine
         self.uspjeh=uspjeh
+        self.karakter=karakter
 
 ucenik1=Ucenik("Josip", 15, 10)
 print(f"Ucenik {ucenik1.name} ima {ucenik1.godine} godina i uspjeh {ucenik1.uspjeh}")
